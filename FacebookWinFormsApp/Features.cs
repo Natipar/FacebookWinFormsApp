e21ec6a17b91cb484fb8ea8d228a097f02f257bb
Nati Parnes <parnes.nati@gmail.com>
2022-03-16 10:17:53 +0200

    Add project files.

---
+++ FacebookWinFormsApp/Features.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+
+    public class Features
+    {
+
+        private NearestFriends m_CloseFriends;
+        private GenderSorter m_GenderSorter;
+        private User m_UserInLogged;
+
+        public Features(User i_UserLogged)
+        {
+
+            m_UserInLogged = i_UserLogged;
+
+        }
+
+        public List<NearestFriends.FriendByCoordinate> InitiateCloseFriends()
+        {
+
+            m_CloseFriends = new NearestFriends(m_UserInLogged);
+            return m_CloseFriends.NearestFriendsMeth();
+
+        }
+
+        public List<User> InitiateGenderSorter(User.eGender i_Gender)
+        {
+            m_GenderSorter = new GenderSorter(m_UserInLogged);
+
+            if (i_Gender == User.eGender.male)
+            {
+
+                return m_GenderSorter.MaleSorter();
+
+            }
+            else
+            {
+                return m_GenderSorter.FemaleSorter();
+            }
+
+        }
+
+    }
+
+}
---
+++ FacebookWinFormsApp/Login.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+    public partial class Login : Form
+    {
+
+        private User m_LoggedInUser;
+        private LoginResult m_LoginResult;
+        private Form1 m_ReloadFormMain;
+
+        public Login()
+        {
+
+            InitializeComponent();
+            FacebookService.s_CollectionLimit = 200;
+            
+
+        }
+
+        private void LoginToForm1()
+        {
+            try
+            {
+                m_LoginResult = FacebookService.Login("322227853207762",
+                            "public_profile",
+                            "email",
+                            "publish_to_groups",
+                            "user_birthday",
+                            "user_age_range",
+                            "user_gender",
+                            "user_link",
+                            "user_videos",
+                            "publish_to_groups",
+                            "groups_access_member_info",
+                            "user_friends",
+                            "user_events",
+                            "user_likes",
+                            "user_location",
+                            "user_photos",
+                            "user_posts",
+                            "user_hometown");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+
+            }
+            if (!string.IsNullOrEmpty(m_LoginResult.AccessToken))
+            {
+
+                m_LoggedInUser = m_LoginResult.LoggedInUser;
+
+            }
+            else
+            {
+
+                MessageBox.Show(m_LoginResult.ErrorMessage);
+
+            }
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            LoginToForm1();
+            this.Hide();
+            m_ReloadFormMain = new Form1(m_LoggedInUser, m_LoginResult);
+            m_ReloadFormMain.ShowDialog();
+            this.Visible = false;
+
+
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+
+            base.OnShown(e);
+            Form1.s_AppSettingss = AppSettings.LoadFromFile();
+
+            if (Form1.s_AppSettingss.RememberUser && !string.IsNullOrEmpty(Form1.s_AppSettingss.RecentAccessToken))
+            {
+
+                m_LoginResult = FacebookService.Connect(Form1.s_AppSettingss.RecentAccessToken);
+                this.Hide();
+                m_LoggedInUser = m_LoginResult.LoggedInUser;
+                m_ReloadFormMain = new Form1(m_LoggedInUser, m_LoginResult);
+                m_ReloadFormMain.ShowDialog();
+
+            }
+
+        }
+
+        private void cbRememberMe_CheckedChanged(object sender, EventArgs e)
+        {
+
+            bool buttonRemmberMe = false;
+
+            if (cbRememberMe.Checked)
+            {
+
+                Form1.s_AppSettingss.RememberUser = !buttonRemmberMe;
+
+            }
+
+        }
+
+    }
+
+}
